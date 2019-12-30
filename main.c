3f5d5fa50be3481de094e92c68561e99fe4045ec
izabelkawl <37289552+izabelkawl@users.noreply.github.com>
2019-12-30 11:49:54 +0100

    Update main.c

--- main.c
+++ main.c
@@ -12,18 +12,15 @@ int main(int argc, char *argv[]) {
 	int first_value, second_value, sum;
 	
 	int odwroc_liczbe(int liczba){
-			    int odwr = floor(log10(liczba)+1);
-			    int wynik=0;
+		int odwr = floor(log10(liczba)+1);
+		int wynik=0;
 			
-			    for(i=1; i<=odwr; i++)
-			    {
-			        wynik += floor(pow(10,odwr-i))* (liczba%10);
-			
-			        liczba /=10;
-			    }
-			
-			    return wynik;
-			}	
+			for(i=1; i<=odwr; i++){
+			wynik += floor(pow(10,odwr-i))* (liczba%10);
+			liczba /=10;
+			}
+		return wynik;
+		}	
 	
 	printf("a = ");
 	scanf("%d", &first_value);
@@ -32,7 +29,7 @@ int main(int argc, char *argv[]) {
 	scanf("%d", &second_value);
 	
 	sum = first_value + second_value;
-	//odwrócenie liczb
+	//odwrÃ³cenie liczb
 	int rev_f = odwroc_liczbe(first_value);
 	int rev_s = odwroc_liczbe(second_value);
 	int rev_o = odwroc_liczbe(sum);
@@ -45,19 +42,17 @@ int main(int argc, char *argv[]) {
 	char suma[10];
 	itoa(rev_o,suma,10);
 	
-	//d³ugoœæ
+	//dÅ‚ugoÅ›Ä‡
 	int fv = floor(log10(first_value)+1);
 	int sv = floor(log10(second_value)+1);
 	int s = floor(log10(sum)+1);
 	
 	if(fv>=sv){
 		max = fv;
-		
 		max_v = first_value;
 		min_v = second_value;
 		}else{
 			max = sv;
-			
 			min_v = first_value;
 			max_v = second_value;
 		}
@@ -65,7 +60,7 @@ int main(int argc, char *argv[]) {
 	//jedynki
 	printf("\n");
 	for (i=max;i>=0;i--){
-		
+		//zamiana char na int
 		int m = jedynki[i] - '0';
 		int n = dwojki[i] - '0';
 		int o = suma[i] - '0';
@@ -81,13 +76,15 @@ int main(int argc, char *argv[]) {
 	printf("  %*d\n",max, max_v);
 	printf("+ %*d\n",max, min_v);
 	
-	 for(i=0;i<(max+2);i++){
-                printf("-");
-        }
+	for(i=0;i<(max+2);i++){
+        printf("-");
+    }
+    
     printf("\n");
     if(s>max){
     	printf(" %*d\n",s, sum);
 	}else{
 		printf("  %*d\n",max, sum);
-	}     	
+	}   
+	system("pause");  	
 }
